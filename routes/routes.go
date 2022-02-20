9eac769aedaf325db0aae1a77087cbd49955af80
Hygor Martins <hygor.martins74@gmail.com>
2022-02-20 15:20:09 -0300

    first commit

---
+++ routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/hygorm10/go-rest-api/controllers"
+	"github.com/hygorm10/go-rest-api/middleware"
+)
+
+func HandleRequest() {
+	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeModdleware)
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriarPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletarPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.AtualizarPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
+}
